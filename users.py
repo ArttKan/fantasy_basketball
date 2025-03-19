b2c0f4650d5140902a3fc2035b4ca41be4c29d6a
Arttu Kankaanpää <arttu.kankaanpaa@gmail.com>
2025-03-19 14:51:40 +0200

    Refactoring of user functions

--- users.py
+++ users.py
@@ -1,4 +1,5 @@
 import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def get_user(user_id):
@@ -15,3 +16,22 @@ def get_teams(user_id):
                 FROM teams
                 WHERE teams.owner = ?"""
     return db.query(sql, [user_id])
+
+
+def create_user(username, password):
+    password_hash = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+    db.execute(sql, [username, password_hash])
+
+
+def check_login(username, password):
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])[0]
+    if not result:
+        return None
+    user_id = result["id"]
+    password_hash = result["password_hash"]
+    if check_password_hash(password_hash, password):
+        return user_id
+    else:
+        return None
