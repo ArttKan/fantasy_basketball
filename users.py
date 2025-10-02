5d68bfaf7a701c7a60d982d83d9b58b3a28a0e09
Arttu Kankaanpää <arttu.kankaanpaa@gmail.com>
2025-10-02 14:45:11 +0300

    added unhashed passwords

--- users.py
+++ users.py
@@ -18,10 +18,14 @@ def get_teams(user_id):
     return db.query(sql, [user_id])
 
 
+# def create_user(username, password):
+#     password_hash = generate_password_hash(password)
+#     sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+#     db.execute(sql, [username, password_hash])
+
 def create_user(username, password):
-    password_hash = generate_password_hash(password)
     sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-    db.execute(sql, [username, password_hash])
+    db.execute(sql, [username, password])
 
 
 def check_login(username, password):
